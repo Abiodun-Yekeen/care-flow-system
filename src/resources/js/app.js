dc4601b8cc7b9d657de3378846ab31bbce6ca0b9
abbeylink <abbeylink@outlook.com>
2026-04-12 06:45:20 +0100

    fix: provide ziggy data to ziggyvue plugin

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -5,6 +5,7 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from 'ziggy-js';
+import { Ziggy } from './ziggy';
 import { createPinia } from 'pinia';
 import { useInertiaSync } from "@/Composables/auth/useInertiaSync.js";
 
@@ -19,19 +20,14 @@ createInertiaApp({
             import.meta.glob('./Pages/**/*.vue'),
         ),
     setup({ el, App, props, plugin }) {
-        // Initialize sync logic
         const { sync } = useInertiaSync();
 
-        // Create the Vue app
         const app = createApp({ render: () => h(App, props) })
             .use(plugin)
-            .use(ZiggyVue)
-            .use(pinia)
+            .use(ZiggyVue, Ziggy)
+            .use(pinia);
 
-        // Initialize sync
         sync();
-
-        // Mount the app
         app.mount(el);
 
         return app;
@@ -40,50 +36,3 @@ createInertiaApp({
         color: '#4B5563',
     },
 });
-
-
-
-
-
-
-// import '../css/app.css';
-// import './bootstrap';
-//
-// import { createInertiaApp } from '@inertiajs/vue3';
-// import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-// import { createApp, h } from 'vue';
-// import { ZiggyVue } from 'ziggy-js';
-// import { createPinia } from 'pinia';
-// import Toast from "vue-toastification";
-// import "vue-toastification/dist/index.css";
-// import {useInertiaSync} from "@/Composables/auth/useInertiaSync.js";
-//
-// const pinia = createPinia()
-// const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
-//
-// createInertiaApp({
-//     title: (title) => `${title} - ${appName}`,
-//     resolve: (name) =>
-//         resolvePageComponent(
-//             `./Pages/${name}.vue`,
-//             import.meta.glob('./Pages/**/*.vue'),
-//         ),
-//     setup({ el, App, props, plugin }) {
-//         return createApp({ render: () => h(App, props) })
-//             .use(plugin)
-//             .use(ZiggyVue)
-//             .use(pinia)
-//             .use(Toast, {
-//                 position: "top-right",
-//                 timeout: 7000,
-//                 containerClassName: "my-toast-container",
-//             })
-//         // Initialize the sync logic
-//         const { sync } = useInertiaSync()
-//         sync()
-//             .mount(el);
-//     },
-//     progress: {
-//         color: '#4B5563',
-//     },
-// });
